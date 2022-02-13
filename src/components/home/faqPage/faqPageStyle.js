7352d1a45b8fd425f04fec5ecb9dbd7a549bc15e
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-02-13 22:20:15 +0100

    update faq page style

--- src/components/home/faqPage/faqPageStyle.js
+++ src/components/home/faqPage/faqPageStyle.js
@@ -3,10 +3,19 @@ import styled from "styled-components";
 export const FaqPageContainer = styled.div`
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
+    flex-direction: column;
+  }
 `;
 
 export const Col1 = styled.div`
   width: 27%;
+
+  @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
+    margin-bottom: 4rem;
+    width: 100%;
+  }
   h1 {
     font-size: 3.8rem;
     font-weight: 600;
@@ -14,12 +23,20 @@ export const Col1 = styled.div`
 `;
 export const Col2 = styled.div`
   width: 65%;
+  @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
+    width: 100%;
+    /* border-bottom: 1px solid grey; */
+  }
   h4 {
     font-size: 2.4rem;
     font-weight: 500;
   }
   .sn {
     color: ${({ theme }) => theme.colors.primary3};
+
+    @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
+      margin-right: 10%;
+    }
   }
 
   .question-container {
@@ -70,5 +87,9 @@ export const Col2 = styled.div`
 
   .content {
     width: 100%;
+  }
+  .line {
+    margin: 3.2rem 0;
+    border-bottom: 2px solid #ececf1;
   }
 `;
--- src/components/home/faqPage/index.js
+++ src/components/home/faqPage/index.js
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-  Container,
-  BackdropContainer,
-} from "../../../reuseableComponents/containerStyle";
+import { Container } from "../../../reuseableComponents/containerStyle";
 import { MdClose } from "react-icons/md";
 import { BiPlus } from "react-icons/bi";
 import { FaqPageContainer, Col1, Col2 } from "./faqPageStyle";
@@ -13,103 +10,37 @@ function FaqPage() {
   const toggleDrop = () => {
     setToggle((prevState) => !prevState);
   };
-
-  // const [id, setId] = React.useState(false);
-  // const targetId = () => {
-  //   setId(reviewsData.id.length === 1 ? !id : id);
-  //   console.log(targetId());
-  // };
   return (
-    <BackdropContainer>
-      <Container>
-        <FaqPageContainer>
-          <Col1>
-            <h1>Frequently asked questions</h1>
-            <a href="/contact">Contact us for more info</a>
-          </Col1>
-          <Col2>
-            {reviewsData.map((review) => (
-              <div key={review.id} className="question-container">
-                <div className="no-quest">
-                  <h4 className="sn">0{review.id}</h4>
-                  <div className="quest-ans">
-                    <h4 className="question">{review.question}</h4>
-                    <p
-                      onClick={toggleDrop}
-                      className={toggle ? "answer" : "no-ans"}
-                    >
-                      {review.answer}
-                    </p>
-                  </div>
-                </div>
-                <div className="toggle" onClick={toggleDrop}>
-                  {toggle ? <MdClose /> : <BiPlus />}
-                </div>
-              </div>
-            ))}
-            <hr />
-            {/* <div className="question-container">
+    <Container>
+      <FaqPageContainer>
+        <Col1>
+          <h1>Frequently asked questions</h1>
+          <a href="/contact">Contact us for more info</a>
+        </Col1>
+        <Col2>
+          {reviewsData.map((review) => (
+            <div key={review.id} className="question-container">
               <div className="no-quest">
-                <h4 className="sn">01</h4>
+                <h4 className="sn">0{review.id}</h4>
                 <div className="quest-ans">
-                  <h4 className="question">How much time does it take?</h4>
+                  <h4 className="question">{review.question}</h4>
                   <p
                     onClick={toggleDrop}
-                    className={onClick ? "answer" : "no-ans"}
+                    className={toggle ? "answer" : "no-ans"}
                   >
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
+                    {review.answer}
                   </p>
+                  <div className="line"></div>
                 </div>
               </div>
               <div className="toggle" onClick={toggleDrop}>
-                {onClick ? <MdClose /> : <BiPlus />}
+                {toggle ? <MdClose /> : <BiPlus />}
               </div>
             </div>
-            <hr /> */}
-            {/* <div className="content">
-              <div className="question-container">
-                <div className="no-quest">
-                  <p>01</p>
-                  <div className="quest-ans">
-                    <h4 className="question">How much time does it take?</h4>
-                  </div>
-                </div>
-                <div className="toggle" onClick={toggleDrop}>
-                  {onClick ? "x" : "+"}
-                </div>
-              </div>
-              <div className="answer-container">
-                <p
-                  onClick={toggleDrop}
-                  className={onClick ? "answer" : "no-ans"}
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-            </div>
-            <hr /> */}
-            {/* <div className="container">
-              <div className="no-quest">
-                <p>01</p>
-
-                <div className="quest-ans">
-                  <h4 className="question">How much time does it take?</h4>
-                  <p className="answer">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p>
-                </div>
-              </div>
-              +
-            </div> */}
-          </Col2>
-        </FaqPageContainer>
-      </Container>
-    </BackdropContainer>
+          ))}
+        </Col2>
+      </FaqPageContainer>
+    </Container>
   );
 }
 
