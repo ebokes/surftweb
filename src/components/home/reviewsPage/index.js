9eb724176b84b96e2200fdd9b837dd3f3939256a
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-02-13 21:32:38 +0100

    update review page style

--- src/components/home/reviewsPage/index.js
+++ src/components/home/reviewsPage/index.js
@@ -3,12 +3,14 @@ import {
   Container,
   BackdropContainer,
 } from "../../../reuseableComponents/containerStyle";
-
 import { ReviewsPageContainer, Col1, Col2 } from "./reviewsPageStyle";
+import woman from "../../../images/woman.png";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { GoChevronLeft } from "react-icons/go";
 
 function ReviewsPage() {
   return (
-    <BackdropContainer>
+    <BackdropContainer bg="#F4F6FC80">
       <Container>
         <ReviewsPageContainer>
           <Col1>
@@ -16,11 +18,24 @@ function ReviewsPage() {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</p>
           </Col1>
           <Col2>
+            <h1>
+              "The best agency we’ve worked with so far. They understand our
+              product and are able to add new features with a great focus."
+            </h1>
             <div>
-              <h1>
-                "The best agency we’ve worked with so far. They understand our
-                product and are able to add new features with a great focus."
-              </h1>
+              <div className="left">
+                <div className="img-container">
+                  <img src={woman} alt="" />
+                </div>
+                <div className="text-container">
+                  <h5>Jenny Wilson</h5>
+                  <p>Vice President</p>
+                </div>
+              </div>
+              <div className="right">
+                <GoChevronLeft color="#28293999" size={36} />
+                <IoIosArrowDroprightCircle color="#5239FA" size={48} />
+              </div>
             </div>
           </Col2>
         </ReviewsPageContainer>
--- src/components/home/reviewsPage/reviewsPageStyle.js
+++ src/components/home/reviewsPage/reviewsPageStyle.js
@@ -3,7 +3,76 @@ import styled from "styled-components";
 export const ReviewsPageContainer = styled.div`
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
+    flex-direction: column;
+  }
 `;
 
-export const Col1 = styled.div``;
-export const Col2 = styled.div``;
+export const Col1 = styled.div`
+  width: 30%;
+
+  @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
+    margin-bottom: 4rem;
+    width: 100%;
+  }
+  & > h1 {
+    font-size: 3.8rem;
+    font-weight: 600;
+    line-height: 5.6rem;
+  }
+
+  & > p {
+    font-size: 1.6rem;
+    font-weight: 400;
+    line-height: 2.8rem;
+    opacity: 0.7;
+  }
+`;
+export const Col2 = styled.div`
+  width: 65%;
+
+  @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
+    flex-direction: column;
+    width: 100%;
+  }
+
+  h1 {
+    font-size: 3.2rem;
+    font-weight: 500;
+    line-height: 4.8rem;
+  }
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left {
+      display: flex;
+      margin-top: 3rem;
+      align-items: center;
+
+      .img-container {
+        img {
+          border-radius: 50%;
+        }
+      }
+      .text-container {
+        margin-left: 1rem;
+        h5 {
+          font-size: 1.8rem;
+          font-weight: 500;
+        }
+        p {
+          font-size: 1.2rem;
+          font-weight: 500;
+          line-height: 2.8rem;
+        }
+      }
+    }
+  }
+
+  .right {
+    display: flex;
+    align-items: center;
+  }
+`;
