78c3df316ebf0eaa285c3c09fda44149f062c950
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-02-19 03:20:33 +0100

    create benefits section

---
+++ src/data/benefitsSectionData.js
@@ -0,0 +1,22 @@
+import client from "../images/client.svg";
+import hands from "../images/hands.svg";
+import customization from "../images/customization.svg";
+
+const benefitsData = [
+  {
+    icon: hands,
+    title: "Customize with ease",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim. ",
+  },
+  {
+    icon: customization,
+    title: "Perfectly Responsive",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim. ",
+  },
+  {
+    icon: client,
+    title: "Friendly Support",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim. ",
+  },
+];
+export default benefitsData;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,6 +2,7 @@ import React from "react";
 import Intro from "../components/aboutus/intro/index";
 import WhoWeAre from "../components/aboutus/whoWeAre/index";
 import Goals from "../components/aboutus/goals/index";
+import Benefits from "../components/aboutus/benefits";
 
 const AboutUs = () => {
   return (
@@ -9,6 +10,7 @@ const AboutUs = () => {
       <Intro />
       <WhoWeAre />
       <Goals />
+      <Benefits />
     </div>
   );
 };
