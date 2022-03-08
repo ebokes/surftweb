338ea4a8821b46382a576e54aeffe48d6d1c6e30
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-03-08 11:33:06 +0100

    modify homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import LandingPage from "../components/home/landingPage/index";
 import WorkPage from "../components/home/workPage/index";
 import ProjectPage from "../components/home/projectPage/index";
 import FeaturesPage from "../components/home/featuresPage/index";
-import ReviewsPage from "../components/home/reviewsPage/index";
 import FaqPage from "../components/home/faqPage/index";
 import ContactForm from "../components/home/contactForm/index";
 
@@ -16,7 +15,6 @@ const Home = () => {
       <WorkPage />
       <ProjectPage />
       <FeaturesPage />
-      <ReviewsPage />
       <FaqPage />
       <ContactForm />
       <BlogPage />
